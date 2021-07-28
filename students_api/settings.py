eaf27a891bacefe207fccf20cfc44389bd5b154a
Ayodabo Oluwatomisin <54403209+king-tomi@users.noreply.github.com>
2021-07-28 20:26:02 +0100

    Add files via upload

--- students_api/settings.py
+++ students_api/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-import django_heroku
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -147,6 +145,5 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:8000',
 )
 
-django_heroku.settings(locals())
 
 ALLOWED_HOSTS = ["studentsapiv1.herokuapp.com"]
